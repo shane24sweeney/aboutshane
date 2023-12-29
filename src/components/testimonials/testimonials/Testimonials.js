b570f3d4f162e51b9d4f5e4dee6f3d5bd238e51f
Shane Sweeney <shane24sweeney@gmail.com>
2023-12-29 11:59:15 -0500

    adding more changes

--- src/components/testimonials/testimonials/Testimonials.js
+++ src/components/testimonials/testimonials/Testimonials.js
@@ -47,12 +47,12 @@ const theme = createTheme({
 
             <img src ={testimonials[0].image} alt={testimonials[0].title}/>
            
-           <Typography variant="h1" >
+           <Typography>
            {testimonials[0].title}
            </Typography>
         
           <div >
-        <span>   <Typography variant="h2">
+        <span>   <Typography>
         {testimonials[0].recommendation}
            </Typography> </span>
        
@@ -66,12 +66,12 @@ const theme = createTheme({
         <testimonials image={testimonials[1].image} centered title={testimonials[1].title} description = {testimonials[0].description}/>
 
 <img src ={testimonials[1].image} alt={testimonials[1].title}/>
-       <Typography variant="h1" gutterBottom>
+       <Typography variant="p">
        {testimonials[1].title}
            </Typography>
         
           <div >
-        <span>   <Typography variant="h2" gutterBottom>
+        <span>   <Typography variant="p">
         {testimonials[1].recommendation}
            </Typography> </span>
        
@@ -83,12 +83,12 @@ const theme = createTheme({
         <testimonials image={testimonials[2].image} title={testimonials[2].title} description = {testimonials[2].description}/>
 
         <img src ={testimonials[2].image} alt={testimonials[2].title}/>
-        <Typography variant="h1" gutterBottom>
+        <Typography>
            {testimonials[2].title}
            </Typography>
         
           <div >
-        <span>   <Typography variant="h2" gutterBottom>
+        <span>   <Typography>
            {testimonials[2].recommendation}
            </Typography> </span>
        
@@ -100,12 +100,12 @@ const theme = createTheme({
         <testimonials image={testimonials[3].image} title={testimonials[3].title} description = {testimonials[3].description}/>
 
 <img src ={testimonials[3].image} alt={testimonials[3].title}/>
-       <Typography variant="h1" gutterBottom>
+       <Typography>
            {testimonials[3].title}
            </Typography>
         
           <div >
-        <span>   <Typography variant="h2" gutterBottom>
+        <span>   <Typography>
            {testimonials[3].recommendation}
            </Typography> </span>
        
@@ -117,12 +117,12 @@ const theme = createTheme({
         <testimonials image={testimonials[4].image} title={testimonials[4].title} description = {testimonials[4].description}/>
 
 <img src ={testimonials[4].image} alt={testimonials[4].title}/>
-       <Typography variant="h1" gutterBottom>
+       <Typography>
            {testimonials[4].title}
            </Typography>
         
           <div >
-        <span>   <Typography variant="h2" gutterBottom>
+        <span>   <Typography>
            {testimonials[4].recommendation}
            </Typography> </span>
        
@@ -134,12 +134,12 @@ const theme = createTheme({
         <testimonials image={testimonials[5].image} title={testimonials[5].title} description = {testimonials[5].description}/>
 
 <img src ={testimonials[5].image} alt={testimonials[5].title}/>
-      <Typography variant="h1" gutterBottom>
+      <Typography>
            {testimonials[5].title}
            </Typography>
         
           <div >
-        <span>   <Typography variant="h2" gutterBottom>
+        <span>   <Typography>
            {testimonials[5].recommendation}
            </Typography> </span>
        
@@ -150,12 +150,12 @@ const theme = createTheme({
         <Carousel.Item interval={4000}>
        
           
-        <Typography variant="h1" gutterBottom>
+        <Typography>
            {testimonials[6].title}
            </Typography>
         
           <div >
-        <span>   <Typography variant="h2" gutterBottom>
+        <span>   <Typography>
            {testimonials[6].recommendation}
            </Typography> </span>
        
@@ -167,12 +167,12 @@ const theme = createTheme({
         <testimonials image={testimonials[7].image} title={testimonials[7].title} description = {testimonials[7].description}/>
 
 <img src ={testimonials[7].image} alt={testimonials[7].title}/>
-      <Typography variant="h1" gutterBottom>
+      <Typography>
            {testimonials[7].title}
            </Typography>
         
           <div >
-        <span>   <Typography variant="h2" gutterBottom>
+        <span>   <Typography>
            {testimonials[7].recommendation}
            </Typography> </span>
        
@@ -184,12 +184,12 @@ const theme = createTheme({
         <testimonials image={testimonials[8].image} title={testimonials[8].title} description = {testimonials[8].description}/>
 
 <img src ={testimonials[8].image} alt={testimonials[8].title}/>
-      <Typography variant="h1" gutterBottom>
+      <Typography>
            {testimonials[8].title}
            </Typography>
         
           <div >
-        <span>   <Typography variant="h2" gutterBottom>
+        <span>   <Typography>
            {testimonials[8].recommendation}
            </Typography> </span>
        
@@ -201,12 +201,12 @@ const theme = createTheme({
         <testimonials image={testimonials[9].image} title={testimonials[9].title} description = {testimonials[9].description}/>
 
 <img src ={testimonials[9].image} alt={testimonials[9].title}/>
-     <Typography variant="h1" gutterBottom>
+     <Typography>
            {testimonials[9].title}
            </Typography>
         
           <div >
-        <span>   <Typography variant="h2" gutterBottom>
+        <span>   <Typography>
            {testimonials[9].recommendation}
            </Typography> </span>
        
