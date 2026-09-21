22a8bb6556f197d098ac44676ec9407706e58e69
Shane Sweeney <shane24sweeney@gmail.com>
2026-09-21 14:11:38 -0400

    Use icon navigation on mobile

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -9,40 +9,37 @@ function Navigation() {
 
   return (
     <div>
-      <Navbar className="site-nav" bg="dark" variant="dark" expand="md">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="site-nav-links mx-auto">
-            <Nav.Link as={NavLink} to="/home" className="text-center mx-2">
+      <Navbar className="site-nav" bg="dark" variant="dark">
+        <Nav className="site-nav-links mx-auto">
+            <Nav.Link as={NavLink} to="/home" className="text-center" title="Home" aria-label="Home">
               <FontAwesomeIcon icon={faHome} size='2x' style={{ color: 'white' }} />
-              <div style={{ marginTop: '5px', color: 'white' }}>Home</div>
+              <span className="nav-label">Home</span>
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/contact" className="text-center mx-2">
+            <Nav.Link as={NavLink} to="/contact" className="text-center" title="Contact" aria-label="Contact">
               <FontAwesomeIcon icon={faAddressCard} size='2x' style={{ color: 'white' }} />
-              <div style={{ marginTop: '5px', color: 'white' }}>Contact</div>
+              <span className="nav-label">Contact</span>
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/about" className="text-center mx-2">
+            <Nav.Link as={NavLink} to="/about" className="text-center" title="About" aria-label="About">
               <FontAwesomeIcon icon={faChess} size='2x' style={{ color: 'white' }} />
-              <div style={{ marginTop: '5px', color: 'white' }}>About</div>
+              <span className="nav-label">About</span>
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/resume" className="text-center mx-2">
+            <Nav.Link as={NavLink} to="/resume" className="text-center" title="Resume" aria-label="Resume">
               <FontAwesomeIcon icon={faAddressBook} size='2x' style={{ color: 'white' }} />
-              <div style={{ marginTop: '5px', color: 'white' }}>Resume</div>
+              <span className="nav-label">Resume</span>
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/testimonials" className="text-center mx-2">
+            <Nav.Link as={NavLink} to="/testimonials" className="text-center" title="Testimonials" aria-label="Testimonials">
               <FontAwesomeIcon icon={faMedal} size='2x' style={{ color: 'white' }} />
-              <div style={{ marginTop: '5px', color: 'white' }}>Testimonials</div>
+              <span className="nav-label">Testimonials</span>
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/education" className="text-center mx-2">
+            <Nav.Link as={NavLink} to="/education" className="text-center" title="Education" aria-label="Education">
               <FontAwesomeIcon icon={faUniversity} size='2x' style={{ color: 'white' }} />
-              <div style={{ marginTop: '5px', color: 'white' }}>Education</div>
+              <span className="nav-label">Education</span>
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/charity" className="text-center mx-2">
+            <Nav.Link as={NavLink} to="/charity" className="text-center" title="Charity Work" aria-label="Charity Work">
               <FontAwesomeIcon icon={faHeart} size='2x' style={{ color: 'white' }} />
-              <div style={{ marginTop: '5px', color: 'white' }}>Charity Work</div>
+              <span className="nav-label">Charity Work</span>
             </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        </Nav>
       </Navbar>
     </div>
   )
